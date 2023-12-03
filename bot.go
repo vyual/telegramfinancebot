0d73e4c865d9213ad807b90d508e9d2dd31c3062
vyual <i@alisoff.ru>
2023-12-03 23:05:47 +0300

    Initial commit

---
+++ bot.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/mymmrac/telego"
+	th "github.com/mymmrac/telego/telegohandler"
+	tu "github.com/mymmrac/telego/telegoutil"
+)
+
+func main() {
+	botToken := "6752659854:AAE5DFiqGadpQd8WckzQkdKMlnQQrET_mug"
+
+	// Note: Please keep in mind that default logger may expose sensitive information,
+	// use in development only
+	bot, err := telego.NewBot(botToken, telego.WithDefaultDebugLogger())
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// Get updates channel
+	updates, _ := bot.UpdatesViaLongPolling(nil)
+
+	// Create bot handler and specify from where to get updates
+	bh, _ := th.NewBotHandler(bot, updates)
+
+	// Stop handling updates
+	defer bh.Stop()
+
+	// Stop getting updates
+	defer bot.StopLongPolling()
+
+	// Register new handler with match on command `/start`
+	bh.HandleMessage(func(bot *telego.Bot, message telego.Message) {
+		// Send a message with inline keyboard
+		_, _ = bot.SendMessage(tu.Message(
+			tu.ID(message.Chat.ID),
+			fmt.Sprintf("Hello %s!", message.From.FirstName),
+		).WithReplyMarkup(tu.InlineKeyboard(
+			tu.InlineKeyboardRow(
+				tu.InlineKeyboardButton("Go!").WithCallbackData("go"),
+			)),
+		))
+	}, th.CommandEqual("start"))
+
+	// Register new handler with match on the call back query
+	// with data equal to `go` and non-nil message
+	bh.HandleCallbackQuery(func(bot *telego.Bot, query telego.CallbackQuery) {
+		// Send message
+		_, _ = bot.SendMessage(tu.Message(tu.ID(query.Message.Chat.ID), "GO GO GO"))
+
+		// Answer callback query
+		_ = bot.AnswerCallbackQuery(tu.CallbackQuery(query.ID).WithText("Done"))
+	}, th.AnyCallbackQueryWithMessage(), th.CallbackDataEqual("go"))
+
+	// Start handling updates
+	bh.Start()
+}
